366e1495d78d84c2ed0d9e2094b2b8946dc7c416
BosSaksit <saksit.kku@gmail.com>
2019-08-26 17:07:00 +0700

    API create models product

---
+++ AMApi/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace AMApi.Models
+{
+    public class Product
+    {
+        public string IdProduct { get; set; }
+        public string NameProduct { get; set; }
+        public string TypeProduct { get; set; }
+        public string PriceProduct { get; set; }
+    }
+}
