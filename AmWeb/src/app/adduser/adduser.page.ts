fcbb34db3097513a24a31ead01125556eb6368dd
rangsiya tunmoon <nameterzaza55@hotmail.com>
2019-10-07 22:00:55 +0700

    AmWeb insert update delete select

--- AmWeb/src/app/adduser/adduser.page.ts
+++ AmWeb/src/app/adduser/adduser.page.ts
@@ -4,41 +4,70 @@ import { user } from 'src/Models/user';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthenService } from '../authen.service';
 @Component({
   selector: 'app-adduser',
   templateUrl: './adduser.page.html',
   styleUrls: ['./adduser.page.scss'],
 })
 export class AdduserPage implements OnInit {
-  dataUser: FormGroup;
-  submit: boolean = false;
-  dataUs: user;
+  // dataUser: FormGroup;
+  // submit: boolean = false;
+  // dataUs: user;
 
-  constructor(public userApi: UserService, public route: Router, public navCtrl: NavController, public formbuilder: FormBuilder) {
-    this.dataUser = this.formbuilder.group({
-      'idUser': [null, Validators.required],
-      'nameUser': [null, Validators.required],
-      'username': [null, Validators.required],
-      'password': [null, Validators.required],
-      'telUser': [null, Validators.required],
-      'levelUser': [null, Validators.required],
-      'addressUser': [null, Validators.required]
+  // constructor(public userApi: UserService, public route: Router, public navCtrl: NavController, public formbuilder: FormBuilder) {
+  //   this.dataUser = this.formbuilder.group({
+  //     'idUser': [null, Validators.required],
+  //     'nameUser': [null, Validators.required],
+  //     'username': [null, Validators.required],
+  //     'password': [null, Validators.required],
+  //     'telUser': [null, Validators.required],
+  //     'levelUser': [null, Validators.required],
+  //     'addressUser': [null, Validators.required]
 
-    })
-  }
-  get f() { return this.dataUser.controls; }
-  
-  async log() {
-    this.submit = true;
-    console.log(this.dataUser.value);
-    console.log(this.dataUser);
-    this.dataUs = this.dataUser.value;
-    this.userApi.AddDataUser(this.dataUs).subscribe(it => {
-      console.log(it);
-    });
-  }
-  
+  //   })
+  // }
+  // get f() { return this.dataUser.controls; }
+
+  // async log() {
+  //   this.submit = true;
+  //   console.log(this.dataUser.value);
+  //   console.log(this.dataUser);
+  //   this.dataUs = this.dataUser.value;
+  //   this.userApi.AddDataUser(this.dataUs).subscribe(it => {
+  //     console.log(it);
+  //   });
+  // }
+
+  // ngOnInit() {
+  // }
+
+  DataUser = {
+    "users_id": "",
+    "user_name": "",
+    "u_username": "",
+    "u_password": "",
+    "user_tel": "",
+    "user_level": "",
+    "user_address": ""
+
+  };
+
+  constructor(public auth: AuthenService,public router:Router) { }
+
   ngOnInit() {
+  }
+
+  addDataUser(){
+    console.log(this.DataUser);
+    let dataFrom = new FormData();
+    dataFrom.append("_Data",JSON.stringify(this.DataUser));
+    dataFrom.append("Function_Name","addUser");
+    console.log(dataFrom);
+    this.auth.callApiUser(dataFrom).then((res)=>{
+      console.log("sussess");
+      
+    }); 
   }
 
 }
--- AmWeb/src/app/user/user.page.ts
+++ AmWeb/src/app/user/user.page.ts
@@ -4,6 +4,7 @@ import { user } from 'src/Models/user';
 import { NavController } from '@ionic/angular';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthenService } from '../authen.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.page.html',
@@ -11,29 +12,79 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class UserPage implements OnInit {
 
-  public dataUserAll: user;
-  constructor(public activate: ActivatedRoute, public userApi: UserService, public route: Router, public navCtrl: NavController, public formbuilder: FormBuilder) { }
+  listUser: any;
+  DataUser = {
+    "users_id": "",
+    "user_name": "",
+    "u_username": "",
+    "u_password": "",
+    "user_tel": "",
+    "user_level": "",
+    "user_address": ""
+
+  };
+
+  constructor(public auth: AuthenService,public router:Router) { }
+
 
   ngOnInit() {
-    this.userApi.GetUserAll().subscribe((it) => {
-      console.log(it);
-      this.dataUserAll = it;
-      console.log(this.dataUserAll);
+    this.getDataUserAll();
+  }
+
+  getDataUserAll() {
+    let dataFrom = new FormData();
+    dataFrom.append("_Data", JSON.stringify(""));
+    dataFrom.append("Function_Name", "getUserAll");
+    this.auth.callApiUser(dataFrom).then((result) => {
+      this.listUser = result;
+      console.log(result);
+      console.log(this.listUser);
     });
   }
 
-  deleteUser(idUser) {
-    this.userApi.DeleteDataUser(idUser).subscribe(it => {
-      this.userApi.GetUserAll().subscribe((it) => {
-        console.log(it);
-        this.dataUserAll = it;
-        console.log(this.dataUserAll);
+  deleteUser(id) {
+    this.DataUser.users_id = id;
+    console.log(this.DataUser.users_id);
+    let dataFrom = new FormData();
+    dataFrom.append("_Data", JSON.stringify(this.DataUser));
+    dataFrom.append("Function_Name", "deleteUser");
+    this.auth.callApiUser(dataFrom).then((result) => {
+      console.log(result);
+      this.getDataUserAll();
 
-      });
     });
+  }
+
+  EditDataUser(id) {
+    this.router.navigate(['/edit-user', { _id: id }]);
   }
   
-  public EditDataUser(id) {
-    this.route.navigate(['/edit-user', { _id: id }]);
-  }
+  // public dataUserAll: user;
+
+  // constructor(public activate: ActivatedRoute, public userApi: UserService, public route: Router, public navCtrl: NavController, public formbuilder: FormBuilder) { }
+
+  // ngOnInit() {
+
+
+  //   // this.userApi.GetUserAll().subscribe((it) => {
+  //   //   console.log(it);
+  //   //   this.dataUserAll = it;
+  //   //   console.log(this.dataUserAll);
+  //   // });
+  // }
+
+  // deleteUser(idUser) {
+  //   this.userApi.DeleteDataUser(idUser).subscribe(it => {
+  //     this.userApi.GetUserAll().subscribe((it) => {
+  //       console.log(it);
+  //       this.dataUserAll = it;
+  //       console.log(this.dataUserAll);
+
+  //     });
+  //   });
+  // }
+
+  // public EditDataUser(id) {
+  //   this.route.navigate(['/edit-user', { _id: id }]);
+  // }
 }
